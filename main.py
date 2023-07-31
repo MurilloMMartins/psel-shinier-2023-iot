f27b0212bfb31b6e4096bcb8d82e6612e8a7c176
MurilloMMartins <murillomartins19@gmail.com>
2023-07-31 17:56:28 -0300

    Testando funcionalidade do GitPython

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import git
+
+def checkForChangesInRemote(repository:git.Repo):
+    currentBranch = repository.active_branch
+
+    localCommits = list(repository.iter_commits(currentBranch))
+    remoteCommits = list(repository.iter_commits(f'origin/{currentBranch}'))
+
+    return localCommits != remoteCommits
+
 def main():
-    print("Testing")
+    repo = git.Repo('./')
+    if checkForChangesInRemote(repo):
+        print("Changes in remote detected!")
+    else:
+        print("Repository up to date.")
+    
 
 if __name__ == "__main__":
     main()
