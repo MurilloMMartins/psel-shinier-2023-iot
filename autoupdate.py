a06430602d65d397089b139a9dc48d23cc28400d
MurilloMMartins <murillomartins19@gmail.com>
2023-08-01 11:10:39 -0300

    Refatorando código de autoatualização

---
+++ autoupdate.py
@@ -0,0 +1,34 @@
+import git
+import subprocess
+
+def CheckForCommitChanges(repository:git.Repo):
+    active_branch = repository.active_branch
+
+    local_commits = list(repository.iter_commits(active_branch))
+    remote_commits = list(repository.iter_commits(f'origin/{active_branch}'))
+
+    last_local_commit = local_commits[0]
+    last_remote_commit = remote_commits[0]
+
+    return last_local_commit != last_remote_commit
+
+def CheckForChangesInRemote(repository:git.Repo):
+    for remote in repository.remotes:
+        remote.fetch()
+    
+    return CheckForCommitChanges(repository)
+
+def PullToLocalRepository(repository:git.Repo):
+    repository.git.reset('--hard')
+    repository.remotes.origin.pull()
+
+def AutoUpdateProgram(repository:git.Repo, main_file_path:str):
+    print(main_file_path)
+    if CheckForChangesInRemote(repository):
+        print("Changes in remote detected! Pulling to local repository.")
+        PullToLocalRepository(repository)
+        
+        print("Restarting process!")
+        subprocess.Popen(['python', main_file_path])
+    else:
+        print("Repository up to date.")
--- main.py
+++ main.py
@@ -1,42 +1,11 @@
 import git
-import subprocess
-
-def checkForCommitChanges(repository:git.Repo):
-    activeBranch = repository.active_branch
-
-    localCommits = list(repository.iter_commits(activeBranch))
-    remoteCommits = list(repository.iter_commits(f'origin/{activeBranch}'))
-
-    lastLocalCommit = localCommits[0]
-    lastRemoteCommit = remoteCommits[0]
-
-    return lastLocalCommit != lastRemoteCommit
-
-def checkForChangesInRemote(repository:git.Repo):
-    for remote in repository.remotes:
-        remote.fetch()
-    
-    return checkForCommitChanges(repository)
-
-def pullToLocalRepository(repository:git.Repo):
-    repository.git.reset('--hard')
-    repository.remotes.origin.pull()
-
-def AutoUpdateProgram(repository:git.Repo):
-    if checkForChangesInRemote(repository):
-        print("Changes in remote detected! Pulling to local repository.")
-        pullToLocalRepository(repository)
-        
-        print("Restarting process!")
-        subprocess.Popen(['python', __file__])
-    else:
-        print("Repository up to date.")
+from autoupdate import AutoUpdateProgram
 
 def main():
     repo = git.Repo('./')
     try:
-        AutoUpdateProgram(repo)
-    except git.GitCommandError as err:
+        AutoUpdateProgram(repo, __file__)
+    except Exception as err:
         print(f"Unexpected error of type {type(err)}:\n {err}")
         exit(1)
 
