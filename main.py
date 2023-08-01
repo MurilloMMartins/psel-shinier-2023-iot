4c4eaeb943dd96ea2308972fac6a004dd8f30298
MurilloMMartins <murillomartins19@gmail.com>
2023-08-01 20:51:24 -0300

    Refatorando código da GUI

--- main.py
+++ main.py
@@ -8,11 +8,47 @@ def onButtonClick():
     popup = tk.Toplevel()
     
     label = tk.Label(popup, text="Candidato processo seletivo Shinier IoT")
-    label.pack(padx=30, pady=10, fill='both', expand=True)
-    
     button = tk.Button(popup, text="OK", command=popup.destroy)
+
+    label.pack(padx=30, pady=10, fill='both', expand=True)
     button.pack(pady=10, padx=10, ipadx=20)
 
+def configureUIResponsivity(root:tk.Tk, frame:tk.Frame):
+    # Configuring root responsivity
+    root.columnconfigure(0, weight=1, minsize=0)
+    root.rowconfigure(0, weight=1, minsize=0)
+    # Configuring frame responsivity
+    frame.columnconfigure(0, weight=1, minsize=0)
+    frame.rowconfigure(0, weight=1, minsize=0)
+    frame.rowconfigure(1, weight=1, minsize=0)
+    frame.rowconfigure(2, weight=1, minsize=0)
+
+def createUIElements(frame:tk.Frame):
+    # UI elements
+    label = ttk.Label(master=frame, text="Nome do candidato")
+    input = ttk.Entry(master=frame)
+    button = ttk.Button(master=frame, text="Inserir", command=onButtonClick)
+
+    return label, input, button
+
+def placeUIElementsOnFrame(frame:tk.Frame, *elements):
+    for index, element, in enumerate(elements):
+        element.grid(column=0, row=index, padx=5, pady=5)
+
+def createApplicatinGUI():
+    # Creating Tkinter Root
+    root = tk.Tk()
+    root.title('Psel-Shinier-2023-IoT')
+    root.geometry('400x225')
+    # Creating frame that will have all UI elements
+    frame = ttk.Frame(master=root, borderwidth=1)
+    frame.grid(column=0, row=0)
+    configureUIResponsivity(root, frame)
+
+    label, input, button = createUIElements(frame)
+    placeUIElementsOnFrame(frame, label, input, button)
+
+    return root
 
 def main():
     # repo = git.Repo('./')
@@ -22,35 +58,7 @@ def main():
     #     print(f"Unexpected error of type {type(err)}:\n {err}")
     #     exit(1)
 
-    # Creating Tkinter Root
-    root = tk.Tk()
-    root.title('Psel-Shinier-2023-IoT')
-    root.geometry('400x225')
-
-    # Creating frame that will have all UI elements
-    frame = ttk.Frame(master=root, borderwidth=1)
-    frame.grid(column=0, row=0)
-
-    # Configuring frame responsivity
-    frame.columnconfigure(0, weight=1, minsize=0)
-    frame.rowconfigure(0, weight=1, minsize=0)
-    frame.rowconfigure(1, weight=1, minsize=0)
-    frame.rowconfigure(2, weight=1, minsize=0)
-
-    # Configuring root responsivity
-    root.columnconfigure(0, weight=1, minsize=0)
-    root.rowconfigure(0, weight=1, minsize=0)
-
-    # UI elements
-    label = ttk.Label(master=frame, text="Nome do candidato")
-    input = ttk.Entry(master=frame)
-    button = ttk.Button(master=frame, text="Inserir", command=onButtonClick)
-
-    # Placing elements on Frame
-    label.grid(column=0, row=0, padx=5, pady=5)
-    input.grid(column=0, row=1, padx=5, pady=5)
-    button.grid(column=0, row=2, padx=5, pady=5)
-
+    root = createApplicatinGUI()
     root.mainloop()
 
 
