cba39d60b5a737d9ec6955671a10846468dacc98
MurilloMMartins <murillomartins19@gmail.com>
2023-08-01 21:05:08 -0300

    Mudando ordem de funções do módulo

--- autoupdate.py
+++ autoupdate.py
@@ -1,27 +1,6 @@
 import git
 import subprocess
 
-def CheckForCommitChanges(repository:git.Repo):
-    active_branch = repository.active_branch
-
-    local_commits = list(repository.iter_commits(active_branch))
-    remote_commits = list(repository.iter_commits(f'origin/{active_branch}'))
-
-    last_local_commit = local_commits[0]
-    last_remote_commit = remote_commits[0]
-
-    return last_local_commit != last_remote_commit
-
-def CheckForChangesInRemote(repository:git.Repo):
-    for remote in repository.remotes:
-        remote.fetch()
-    
-    return CheckForCommitChanges(repository)
-
-def PullToLocalRepository(repository:git.Repo):
-    repository.git.reset('--hard')
-    repository.remotes.origin.pull()
-
 def AutoUpdateProgram(repository:git.Repo, main_file_path:str):
     print(main_file_path)
     if CheckForChangesInRemote(repository):
@@ -32,3 +11,24 @@ def AutoUpdateProgram(repository:git.Repo, main_file_path:str):
         subprocess.Popen(['python', main_file_path])
     else:
         print("Repository up to date.")
+
+def PullToLocalRepository(repository:git.Repo):
+    repository.git.reset('--hard')
+    repository.remotes.origin.pull()
+    
+def CheckForChangesInRemote(repository:git.Repo):
+    for remote in repository.remotes:
+        remote.fetch()
+    
+    return CheckForCommitChanges(repository)
+
+def CheckForCommitChanges(repository:git.Repo):
+    active_branch = repository.active_branch
+
+    local_commits = list(repository.iter_commits(active_branch))
+    remote_commits = list(repository.iter_commits(f'origin/{active_branch}'))
+
+    last_local_commit = local_commits[0]
+    last_remote_commit = remote_commits[0]
+
+    return last_local_commit != last_remote_commit
