20b4c95cce3ab8e355fb7997d1c9604483f60c38
MurilloMMartins <murillomartins19@gmail.com>
2023-08-02 16:19:10 -0300

    Integrando a UI com a função de automaticamente atualizar, além de adicionar funcionalidade de buscar atualização mais de uma vez após um período de tempo

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-import git
+import git, threading, time
 from autoupdate import AutoUpdateProgram
-from applicationGUI import createApplicatinGUI
+from applicationGUI import CreateApplicatinGUI
+
+autoupdate_wait_time = 2
+
+def AutoUpdateThreadFunction(event:threading.Event):
+    while not event.is_set():
+        repo = git.Repo('./')
+
+        try:
+            AutoUpdateProgram(repo, __file__)
+        except Exception as err:
+            print(f"Unexpected error of type {type(err)}:\n {err}")
+            exit(1)
+        
+        event.wait(timeout=autoupdate_wait_time)
 
 def main():
-    # repo = git.Repo('./')
-    # try:
-    #     AutoUpdateProgram(repo, __file__)
-    # except Exception as err:
-    #     print(f"Unexpected error of type {type(err)}:\n {err}")
-    #     exit(1)
+    # Event that tells if the code is still running or not
+    event = threading.Event()
+    event.clear()
 
-    root = createApplicatinGUI()
+    # Autoupdate event thread
+    autoupdate_thread = threading.Thread(target=AutoUpdateThreadFunction, args=(event, ))
+    autoupdate_thread.start()
+
+    # GUI
+    root = CreateApplicatinGUI()
     root.mainloop()
+    
+    # Closing the system
+    event.set()
+    autoupdate_thread.join()
 
 if __name__ == "__main__":
     main()
